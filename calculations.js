a6b20c19a8060abb0a6e5504c89b7a1647d535e2
Lstarinskaya <starinskayaliliia@gmail.com>
2024-01-21 15:35:08 +0300

    fixed functions.js for 2.2.1 task

--- calculations.js
+++ calculations.js
@@ -1,25 +1,28 @@
 function increaseBalance(x, y) {
   x = x + y;
+  console.log(x);
   return x;
 }
 
-function decreaseBalance(x) {
-x = x - y;
-return x;
+function decreaseBalance(x, y) {
+  x = x - y;
+  console.log(x);
+  return x;
 }
 
 function divideBalanceByAccounts(x, y) {
   x = x / y;
+  console.log(x);
   return x;
-  console.log("x");
 }
 
 function getRestAfterDivision(x, y) {
   x = x % y;
+  console.log(x);
   return x;
 }
 
-console.log(increaseBalance(3000, 700));
-console.log(decreaseBalance(3000, 700));
-console.log(divideBalanceByAccounts(3000, 2, 4000));
-console.log(getRestAfterDivision(7000, 3));
+increaseBalance(3000, 700);
+decreaseBalance(3000, 700);
+divideBalanceByAccounts(3000, 2);
+getRestAfterDivision(7000, 3);
