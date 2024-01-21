d96973d26270832f3de41a900c5ceba4a89fa386
Lstarinskaya <starinskayaliliia@gmail.com>
2024-01-21 19:27:27 +0300

    Edited calculations.js for 2.2.4 task

--- calculations.js
+++ calculations.js
@@ -22,7 +22,19 @@ function getRestAfterDivision(x, y) {
   return x;
 }
 
+function increaseUserNumber(x) {
+  ++x;
+  console.log(x);
+}
+
+function decreaseCarNumber(x) {
+  --x;
+  console.log(x);
+}
+
 increaseBalance(3000, 700);
 decreaseBalance(3000, 700);
 divideBalanceByAccounts(3000, 2);
 getRestAfterDivision(7000, 3);
+increaseUserNumber(5);
+decreaseCarNumber(155);
